ef306894e879282e152e697ba007595e081e6229
Francisco Prada <fmamp24@gmail.com>
2022-04-14 20:08:16 +0100

    the program also says how many deliveries are returned

--- Scenario3.cpp
+++ Scenario3.cpp
@@ -110,6 +110,6 @@ float Scenario3::getMeanTime() {
     }
     float mean_time = total/counter;
     std::cout << "the total time spent by the express delivery truck is " << total << " seconds, which is about " << total / 60 << " minutes, or " << total / 60 / 60 << " hours\nThe mean time is about " << mean_time << " seconds or " << mean_time/60 << " minutes." ;
-
+    std::cout << "\nreturning " << size -counter << " deliveries to the provider\n";
     return mean_time;
 }
